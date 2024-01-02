8c98c26cec20674f0141923cbeb1b1380f5145b7
viniciuscmnz <viniciuscassiomnz@gmail.com>
2024-01-02 08:11:09 -0300

    first commit

---
+++ main1.py
@@ -0,0 +1,323 @@
+from tkinter.ttk import Label, Button, Combobox, Style
+from tkinter import messagebox
+from ttkthemes import ThemedTk
+from PIL import Image, ImageTk
+import keyboard
+import pyautogui
+from window import hidden_client
+import json
+import threading
+import pynput
+import time
+import atexit
+import pygetwindow as gw
+
+root = ThemedTk(theme="msc", themebg=True)
+root.title("Macro")
+root.resizable(False, False)
+style = Style()
+style.configure('TButton', font=("Roboto", 12))
+style.configure('Ativado.TButton', foreground="green")
+style.configure('Desativado.TButton', foreground="red")
+
+HOTKEYS = ["Desligado", "F1", "F2", "F3", "F4", "F5", "F6", "F7", "F8", "F9", "F10", "F11", "F12"]
+HUR = ["Desligado", "Utani Hur", "Utani Gran Hur", "Utani Tempo Hur"]
+
+def generate_widget(widget, row, column, sticky="NSEW", columnspan= None, **kwargs):
+    my_widget = widget(**kwargs)
+    my_widget.grid(row=row, column=column, padx=5, pady= 5, columnspan=columnspan, sticky=sticky)
+    return my_widget
+
+def load_trash():
+    load_image = Image.open('trash-icon.jpg')
+    resized_image = load_image.resize((20, 20))
+    return ImageTk.PhotoImage(resized_image)
+
+lbl_healing = generate_widget(Label, row=0, column=0, sticky="W", text="Healing", font=("Roboto", 12))
+lbl_utilities = generate_widget(Label, row=5, column=0, sticky="W", text="Utility", font=("Roboto", 12))
+
+lbl_food = generate_widget(Label, row=6, column=0, sticky="W", text="Eatfood", font=("Roboto", 12))
+cbx_food = generate_widget(Combobox, row=6, column=1, values=HOTKEYS, state="readonly", font=("Roboto", 12), width= 12)
+cbx_food.current(0)
+
+lbl_hur = generate_widget(Label, row=7, column=0, sticky="W", text="Auto Hur (F4)", font=("Roboto", 12))
+cbx_hur = generate_widget(Combobox, row=7, column=1, values=HUR, state="readonly", font=("Roboto", 12), width= 12)
+cbx_hur.current(0)
+
+lbl_cast = generate_widget(Label, row=2, column=0, sticky="W", text="Mana", font=("Roboto", 12))
+cbx_cast = generate_widget(Combobox, row= 2, column=1, values=HOTKEYS, state="readonly", font=("Roboto", 12), width= 12)
+cbx_cast.current(0)
+
+lbl_hp_heal = generate_widget(Label, row=1, column=0, sticky="W", text="HP", font=("Roboto", 12))
+cbx_hp_heal = generate_widget(Combobox, row= 1, column=1, values=HOTKEYS, state="readonly", font=("Roboto", 12), width= 12)
+cbx_hp_heal.current(0)
+
+# Adicionando as novas funções Skill 1 e Skill 2
+lbl_skill1 = generate_widget(Label, row=3, column=0, sticky="W", text="Skill 1", font=("Roboto", 12))
+cbx_skill1 = generate_widget(Combobox, row= 3, column=1, values=HOTKEYS, state="readonly", font=("Roboto", 12), width= 12)
+cbx_skill1.current(0)
+
+lbl_skill2 = generate_widget(Label, row=4, column=0, sticky="W", text="Skill 2", font=("Roboto", 12))
+cbx_skill2 = generate_widget(Combobox, row= 4, column=1, values=HOTKEYS, state="readonly", font=("Roboto", 12), width= 12)
+cbx_skill2.current(0)
+
+rgb = ''
+mana_position = ''
+def get_mana_position():
+    global rgb
+    global mana_position
+    messagebox.showinfo(title="Mana Position", message="Position the mouse over the mana bar and press the insert key")
+    keyboard.wait('insert')
+    x, y = pyautogui.position()
+    rgb = pyautogui.screenshot().getpixel((x, y))
+    messagebox.showinfo(title='Mana Result', message=f"X: {x} Y: {y} - RGB: {rgb}")
+    lbl_mana_position.configure(text=f"({x}, {y})")
+    mana_position = [x, y]
+
+btn_mana_position = generate_widget(Button, row=2, column=3, text= "Mana Position", command=get_mana_position)
+lbl_mana_position = generate_widget(Label, row=2, column=4, text="Empty", font=("Roboto", 12), sticky="W")
+
+hp_rgb = ''
+hp_position = ''
+def get_hp_position():
+    global hp_rgb
+    global hp_position
+    messagebox.showinfo(title="HP Position", message="Position the mouse over the HP bar and press the insert key")
+    keyboard.wait('insert')
+    x, y = pyautogui.position()
+    hp_rgb = pyautogui.screenshot().getpixel((x, y))
+    messagebox.showinfo(title='HP Result', message=f"X: {x} Y: {y} - RGB: {hp_rgb}")
+    lbl_hp_position.configure(text=f"({x}, {y})")
+    hp_position = [x, y]
+
+btn_hp_position = generate_widget(Button, row=1, column=3, text= "HP Position", command=get_hp_position)
+lbl_hp_position = generate_widget(Label, row=1, column=4, text="Empty", font=("Roboto", 12), sticky="W")
+
+# Adicionando as funções para obter a posição das skills
+skill1_rgb = ''
+skill1_position = ''
+def get_skill1_position():
+    global skill1_rgb
+    global skill1_position
+    messagebox.showinfo(title="Skill 1 Position", message="Position the mouse over the Skill 1 bar and press the insert key")
+    keyboard.wait('insert')
+    x, y = pyautogui.position()
+    skill1_rgb = pyautogui.screenshot().getpixel((x, y))
+    messagebox.showinfo(title='Skill 1 Result', message=f"X: {x} Y: {y} - RGB: {skill1_rgb}")
+    lbl_skill1_position.configure(text=f"({x}, {y})")
+    skill1_position = [x, y]
+
+btn_skill1_position = generate_widget(Button, row=3, column=3, text= "Skill 1 Position", command=get_skill1_position)
+lbl_skill1_position = generate_widget(Label, row=3, column=4, text="Empty", font=("Roboto", 12), sticky="W")
+
+skill2_rgb = ''
+skill2_position = ''
+def get_skill2_position():
+    global skill2_rgb
+    global skill2_position
+    messagebox.showinfo(title="Skill 2 Position", message="Position the mouse over the Skill 2 bar and press the insert key")
+    keyboard.wait('insert')
+    x, y = pyautogui.position()
+    skill2_rgb = pyautogui.screenshot().getpixel((x, y))
+    messagebox.showinfo(title='Skill 2 Result', message=f"X: {x} Y: {y} - RGB: {skill2_rgb}")
+    lbl_skill2_position.configure(text=f"({x}, {y})")
+    skill2_position = [x, y]
+
+btn_skill2_position = generate_widget(Button, row=4, column=3, text= "Skill 2 Position", command=get_skill2_position)
+lbl_skill2_position = generate_widget(Label, row=4, column=4, text="Empty", font=("Roboto", 12), sticky="W")
+
+trash = load_trash()
+
+def clear_mana():
+    lbl_mana_position.configure(text="Empty")
+
+def clear_hp():  # Função para limpar a posição do hp
+    lbl_hp_position.configure(text="Empty")
+
+def clear_skill1():  # Função para limpar a posição da skill 1
+    lbl_skill1_position.configure(text="Empty")
+
+def clear_skill2():  # Função para limpar a posição da skill 2
+    lbl_skill2_position.configure(text="Empty")
+
+btn_mana_position_trash = generate_widget(Button, row=2, column=5, image=trash, sticky="E")
+btn_mana_position_trash.configure(command=clear_mana)
+
+btn_hp_position_trash = generate_widget(Button, row=1, column=5, image=trash, sticky="E")  # Botão de limpar para hp
+btn_hp_position_trash.configure(command=clear_hp)
+
+btn_skill1_position_trash = generate_widget(Button, row=3, column=5, image=trash, sticky="E")  # Botão de limpar para skill 1
+btn_skill1_position_trash.configure(command=clear_skill1)
+
+btn_skill2_position_trash = generate_widget(Button, row=4, column=5, image=trash, sticky="E")  # Botão de limpar para skill 2
+btn_skill2_position_trash.configure(command=clear_skill2)
+
+def opacity():
+    result = hidden_client()
+    if result == 1:
+        btn_opacity.configure(style='Ativado.TButton')
+        return
+    btn_opacity.configure(style='Desativado.TButton')
+
+btn_opacity = generate_widget(Button, row=9, column=0, text="Apply Opacity", columnspan=2, command=opacity)
+btn_opacity.configure(style='Desativado.TButton')  # Inicia a função como desativada
+
+def cleanup():
+    btn_opacity.configure(style='Desativado.TButton')  # Desativa a função quando o programa é fechado
+
+atexit.register(cleanup)
+
+def load():
+    with open('infos.json', 'r') as file:
+        data = json.loads(file.read())
+    cbx_food.current(data['food']['position'])
+    cbx_cast.current(data['spell']['position'])
+    cbx_hur.current(data['hur']['position'])
+    cbx_hp_heal.current(data['hp_heal']['position'])
+    cbx_skill1.current(data['skill1']['position'])  # Carregando a posição da skill 1
+    cbx_skill2.current(data['skill2']['position'])  # Carregando a posição da skill 2
+    lbl_mana_position.configure(text=data['mana_pos']['position'])
+    lbl_hp_position.configure(text=data['hp_pos']['position'])  # Carregando a posição do hp
+    lbl_skill1_position.configure(text=data['skill1_pos']['position'])
+    lbl_skill2_position.configure(text=data['skill2_pos']['position'])
+    return data
+
+def save():
+    my_data = {
+        "food": {
+            "value": cbx_food.get(),
+            "position": cbx_food.current()
+        },
+        "hur":{
+            "value": cbx_hur.get(),
+            "position": cbx_hur.current()
+        },        
+        "spell": {
+            "value": cbx_cast.get(),
+            "position": cbx_cast.current()
+        },
+        "hp_heal": {
+            "value": cbx_hp_heal.get(),
+            "position": cbx_hp_heal.current()
+        },
+        "skill1": {  # Salvando a posição da skill 1
+            "value": cbx_skill1.get(),
+            "position": cbx_skill1.current()
+        },
+        "skill2": {  # Salvando a posição da skill 2
+            "value": cbx_skill2.get(),
+            "position": cbx_skill2.current()
+        },
+        "mana_pos": {
+            "position": mana_position,
+            "rgb": rgb
+        },
+        "hp_pos": {  # Salvando a posição do hp
+            "position": hp_position,
+            "rgb": hp_rgb
+        },
+        "skill1_pos": {  # Salvando a posição do hp
+            "position": skill1_position,
+            "rgb": skill1_rgb
+        },
+        "skill2_pos": {  # Salvando a posição do hp
+            "position": skill2_position,
+            "rgb": skill2_rgb
+        }
+    }
+    with open('infos.json', 'w') as file:
+        file.write(json.dumps(my_data))
+
+def run():
+    
+    first_press = True
+
+    wait_to_eat_food = 60
+    time_food = time.time() - wait_to_eat_food
+
+    while not myEvent.is_set():
+        try:
+            tibia = gw.getWindowsWithTitle('Tibia')[0]  # Substitua 'Tibia' pelo título da janela do seu jogo
+            tibia.activate()
+        except IndexError:
+            print("Janela do Tibia não encontrada")
+
+        if data['hur']['value'] != 'Desligado':
+            wait_to_cast_hur = 0
+        if data['hur']['value'] == 'Utani Hur':
+            wait_to_cast_hur = 31
+        elif data['hur']['value'] == 'Utani Gran Hur':
+            wait_to_cast_hur = 20
+        elif data['hur']['value'] == 'Utani Tempo Hur':
+            wait_to_cast_hur = 4
+
+        if first_press or int(time.time() - time_hur) >= wait_to_cast_hur:
+            pyautogui.press('f4')
+            time_hur = time.time()
+            first_press = False
+        if isinstance(data['mana_pos']['position'], list):
+            x = data['mana_pos']['position'][0]
+            y = data['mana_pos']['position'][1]
+            if pyautogui.pixelMatchesColor(x, y, tuple(data['mana_pos']['rgb'])):
+                if data['spell']['value'] != 'Desligado':
+                    pyautogui.press(data['spell']['value'])
+                    time.sleep(0.1) 
+
+        if isinstance(data['hp_pos']['position'], list):  
+            x_hp = data['hp_pos']['position'][0]
+            y_hp = data['hp_pos']['position'][1]
+            if pyautogui.pixelMatchesColor(x_hp, y_hp, tuple(data['hp_pos']['rgb'])):
+                if data['hp_heal']['value'] != 'Desligado':
+                    pyautogui.press(data['hp_heal']['value'])
+                    time.sleep(0.1) 
+
+        if isinstance(data['skill1_pos']['position'], list):
+            x_skill1 = data['skill1_pos']['position'][0]
+            y_skill1 = data['skill1_pos']['position'][1]
+            if pyautogui.pixelMatchesColor(x_skill1, y_skill1, tuple(data['skill1_pos']['rgb'])):
+                if data['skill1']['value'] != 'Desligado':
+                    pyautogui.press(data['skill1']['value'])
+                    time.sleep(0.1) 
+
+        if isinstance(data['skill2_pos']['position'], list):
+            x_skill2 = data['skill2_pos']['position'][0]
+            y_skill2 = data['skill2_pos']['position'][1]
+            if pyautogui.pixelMatchesColor(x_skill2, y_skill2, tuple(data['skill2_pos']['rgb'])):
+                if data['skill2']['value'] != 'Desligado':
+                    pyautogui.press(data['skill2']['value'])
+                    time.sleep(0.1) 
+
+        if data['food']['value'] != 'Desligado':
+            if int(time.time() - time_food) >= wait_to_eat_food:
+                pyautogui.press(data['food']['value'])
+                time_food = time.time()
+
+def key_code(key):
+    if key == pynput.keyboard.Key.esc:
+        myEvent.set()
+        root.deiconify()
+        return False
+
+def listener_keyboard():
+    with pynput.keyboard.Listener(on_press=key_code) as Listener:
+        Listener.join()
+
+def start():
+    root.iconify()
+    save()
+    global data
+    data = load()
+    global myEvent
+    myEvent = threading.Event()
+    global start_th
+    start_th = threading.Thread(target=run)
+    start_th.start()
+    keyboard_th = threading.Thread(target=listener_keyboard)
+    keyboard_th.start()
+
+btn_start = generate_widget(Button, row=10, column=1, text="Start", command=start)
+btn_load = generate_widget(Button, row=10, column=0, text="Load", command=load)
+
+root.mainloop()
+
+
